6e0757305cff98d328ae24bbc7dcb59bdc5d8b55
safikulalam96 <safikulalam9679@gmail.com>
2024-05-28 22:32:51 +0530

    Creating news Web through React

---
+++ src/component/NewsItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let {title,description,imageUrl,newsurl}=this.props;
+    return (
+      <div>
+        <div className="card my-3" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="/" />
+          <div className="card-body">
+            <h5 className="card-title">{title.slice(0,45)}...</h5>
+            <p className="card-text">
+                {description.slice(0,88)}...
+            </p>
+            <a href={newsurl} target="blank" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
+
+// 865b740cf1314a2692aec641f4f6d6a6
